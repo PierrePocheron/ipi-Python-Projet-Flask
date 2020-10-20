a58b51c8624718a5c267a522341d51708bf20e11
PierrePocheron <pierre.pocheron@gmail.com>
2020-10-20 10:33:42 +0200

    Ajout des routes

--- projet-python-flask/hello.py
+++ projet-python-flask/hello.py
@@ -3,7 +3,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!dsdqs'
+    return 'Hello, World!Heeeyy ! Bienvenu sur ce projet !     Le site web vient d être mit à jour !'
 
-print('Heeeyy ! Bienvenu sur ce projet !')
-print('Le site web vient d être mit à jour !')
+
+@app.route('/connexion')
+def hello_world2():
+    return 'Bienvenue sur la page : Connexion !'
+
+
+@app.route('/produit')
+def hello_world3():
+    return 'Bienvenue sur la page : Produit !'
+
+
+@app.route('/panier')
+def hello_world4():
+    return 'Bienvenue sur la page : Panier !'
+
+
