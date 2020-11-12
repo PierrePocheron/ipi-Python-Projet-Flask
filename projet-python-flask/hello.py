968acab42e84a03e4ad6318354ec8a8d607fe4f1
PierrePocheron <pierre.pocheron@gmail.com>
2020-11-12 12:20:16 +0100

    Page produit front OK

--- projet-python-flask/hello.py
+++ projet-python-flask/hello.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-# from flask_bootstrap import Bootstrap
 from db import get_db
 
 app = Flask(__name__)
@@ -9,24 +8,24 @@ def hello_world():
 
 
 @app.route('/connexion')
-def index():
+def connexion():
     return render_template("view_connexion.html")
 
 
 @app.route('/produit')
-def hello_produit():
-    db = get_db()
-    produits = db.execute(
-        'SELECT *'
-        ' FROM produit'
-        ' ORDER BY name'
-    ).fetchall()
+def produit():
+    # db = get_db()
+    # produits = db.execute(
+    #     'SELECT *'
+    #     ' FROM produit'
+    #     ' ORDER BY name'
+    # ).fetchall()
     # return render_template("view_produit.html", produits=produits)
     return render_template("view_produit.html")
 
 
 @app.route('/panier')
-def hello_panier():
+def panier():
     return render_template("view_panier.html")
 
 
