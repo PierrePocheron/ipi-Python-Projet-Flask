406f67793f909d78e4d493cec1871e15b6dc46c4
PierrePocheron <pierre.pocheron@gmail.com>
2020-11-14 21:57:57 +0100

    Systeme d'authentification without bdd

--- projet-python-flask/__init__.py
+++ projet-python-flask/__init__.py
@@ -1,26 +1,15 @@
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, render_template, app, request
+
+import sqlite3
+import os
+
+# from db import get_db
 from . import db
 
 
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/')
-    def hello_world():
-        return render_template("view_connexion.html")
-
-    @app.route('/connexion')
-    def connexion():
-        return render_template("view_connexion.html")
-
-    @app.route('/produit')
-    def produit():
-        return render_template("view_produit.html")
-
-    @app.route('/panier')
-    def panier():
-        return render_template("view_panier.html")
 
     db.init_app(app)
     return app
--- projet-python-flask/hello.py
+++ projet-python-flask/hello.py
@@ -1,5 +1,12 @@
-from flask import Flask, render_template, app, request
-
+from flask import (
+    Flask,
+    redirect,
+    render_template,
+    app,
+    request,
+    session,
+    url_for
+)
 import sqlite3
 import os
 
@@ -8,27 +15,58 @@ from . import db
 
 
 def create_app():
+    class User:
+        def __init__(self, id, username, password):
+            self.id = id
+            self.username = username
+            self.password = password
+
+        def __repr__(self):
+            return f'<User: {self.username}>'
+
+    users = []
+    users.append(User(id=1, username='Pierre', password='Pierre'))
+    users.append(User(id=2, username='root', password='root'))
+
+    print(users)
+
     currentdirectory = os.path.dirname(os.path.abspath(__file__))
     app = Flask(__name__)
+    app.secret_key = 'somesecretkey'
 
-    @app.route('/')
-    def hello_world():
-        return render_template("view_connexion.html")
 
-    @app.route('/connexion')
+    # @app.route('/')
+    # def defaultPage():
+    #     return render_template("view_connexion.html")
+
+    @app.route('/connexion', methods=['GET', 'POST'])
     def connexion():
+        if request.method == 'POST':
+            session.pop('user_id', None)
+
+            result = request.form
+            u = result['username']
+            p = result['password']
+
+            user = [x for x in users if x.username == u][0]
+            if user and user.password == p:
+                session['user_id'] = user.id
+                return redirect(url_for('produit'))
+
+            return redirect(url_for('connexion'))
+
         return render_template("view_connexion.html")
 
     @app.route('/produit')
     def produit():
-        # db = get_db()
-        # produits = db.execute(
-        #     'SELECT *'
-        #     ' FROM produit'
-        #     ' ORDER BY name'
-        # ).fetchall()
-        # return render_template("view_produit.html", produits=produits)
         return render_template("view_produit.html")
+
+    # @app.route("/produit", methods=["POST"])
+    # def userConnected():
+    #     result = request.form
+    #     u = result['username']
+    #     p = result['password']
+    #     return render_template("view_produit.html", username=u, password=p)
 
     @app.route('/panier')
     def panier():
@@ -36,3 +74,26 @@ def create_app():
 
     db.init_app(app)
     return app
+
+# @app.route('/produit')
+#     def produit():
+#         # a l'initialisation, charger les produits
+#         # db = get_db()
+#         # produits = db.execute(
+#         #     'SELECT *'
+#         #     ' FROM produit'
+#         #     ' ORDER BY name'
+#         # ).fetchall()
+#         # return render_template("view_produit.html", produits=produits)
+#         return render_template("view_produit.html")
+
+
+# @app.route("/connexion", methods=["POST"])
+# def addUtilisateur():
+#     username = request.form["Username"]
+#     password = request.form["Password"]
+#     connexion = sqlite3.connect(currentdirectory + "\projet-python-flask.db")
+#     cursor = connexion.cursor()
+#     query = "INSERT INTO Utilisateur VALUES('{use}',{pas})".format(use=username, pas=password)
+#     cursor.execute(query)
+#     connexion.commit()
